c4ddff4480c1f4f089d2b5a31d31886deea4f046
dxqit <dxqit@163.com>
2016-11-23 20:00:34 +0800

    test

--- okhttputils/src/main/java/com/zhy/http/okhttp/OkHttpUtils.java
+++ okhttputils/src/main/java/com/zhy/http/okhttp/OkHttpUtils.java
@@ -140,7 +140,6 @@ public class OkHttpUtils
 
                     if (!finalCallback.validateReponse(response, id))
                     {
-
                         sendFailResultCallback(call, new IOException("request failed , reponse's code is : " + response.code()), finalCallback, id);
                         return;
                     }
